d4bbacc81715679462495b653685760fc09878d8
moselle123 <95174028+moselle123@users.noreply.github.com>
2022-10-06 09:09:02 +0100

    Js added navbar now not working

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-$('#nav-toggle').change(function() {
-	$(#nav).slideDown(500);
-});
+const faders = document.querySelectorAll(".fade-up");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
+  
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
+
+  window.location = $(".myBox").data("location");
